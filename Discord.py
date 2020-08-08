ea93d163eda65967063364d22ff49dc1193ab152
p-chandra <33384192+p-chandra@users.noreply.github.com>
2020-08-08 00:22:11 -0700

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -33,56 +33,23 @@ async def mul(ctx,*args: int):
 async def div(ctx,*args: int):
     await ctx.send(sum(args))
 
-    
 @client.command()
 async def cal(ctx,*args):
-    data = []
-    for i in args:
-        temp = i.split('+')
-    await ctx.send(temp)
-    
-    
-    
-    temp = 0
-    for index, x in enumerate(args):
-    	if (x == '+'):
-    	    temp = int(args[index-1]) + int(args[index+1])
-    	
-    	elif (x == '-'):
-    	    temp = int(args[index-1]) - int(args[index+1])
-    	
-    	elif(x == '*'):
-    	    temp = int(args[index-1]) * int(args[index+1])
-    	
-    	elif(x == '/' and args[index+1] != '0'):
-    	    temp = int(args[index-1]) / int(args[index+1])
-    
-    	else:
-    	    await ctx.send("Invalid Data Entry")
-    	    break
-    await ctx.send(temp)
-
-   @client.command()
-async def space(ctx,*args):
-    queue = []
-    final = []
-    i = 0
-    temp = ""
-    for index, x in enumerate(args):
-        if(index % 2 == 0):
-            queue.append(x)
-        else:
-            queue.append(x)
-    while i < len(queue):
-        if (queue[i] == '+'):
-            temp = queue.pop([i-1]) + queue.pop([i+1])
-            #del queue[i-1]
-            #del queue[i-1]
-            #del queue[i-1]
-            queue.insert(0,temp)
-        i += 1
-    await ctx.send(queue)
-
-        
-        
+    data = list(args)
+    if (len(data) % 2 != 1):
+        await ctx.send("Invalid Number of Arguments")
+    else:
+        for x in data:
+            if (x == '*'):
+                index = data.index('*')
+                temp = float(data.pop(index-1)) * float(data.pop(index))
+                data.remove('*')
+                data.insert(index-1,temp)
+            elif (x == '+'):
+                index = data.index('+')
+                temp = float(data.pop(index-1)) + float(data.pop(index))
+                data.remove('+')
+                data.insert(index-1,temp)
+            await ctx.send(data)
+       
 client.run("")
